df987bf8e3c72f41560110582db08434ac450c5f
Zahar-Borovskiy <borovskiyzahar@gmail.com>
2024-12-09 13:56:36 -0300

    первый коммит

---
+++ weather_parser.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+def weather_parser(city):
+    urls = {
+        'Санкт-Петербург': 'https://rp5.ru/Погода_в_Санкт-Петербурге',
+        'Буэнос-Айрес': 'https://rp5.ru/Погода_в_Буэнос-Айресе'
+    }
+
+    if city not in urls:
+        return f'Город {city} не поддерживается.'
+
+    url = urls[city]
+    class_ = 'ArchiveTemp'
+
+    r = requests.get(url)
+    html = BS(r.text, 'html.parser')
+
+    try:
+        t = html.find(class_=class_).find(class_='t_0').text
+        print(f'Погода в {city}: {t}')
+    except AttributeError:
+        print(f'Не удалось получить данные о погоде для {city}.')
+
+# Примеры вызова функции
+weather_parser('Санкт-Петербург')
+weather_parser('Буэнос-Айрес')
